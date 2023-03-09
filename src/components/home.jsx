b35df41cfc77559a478899fad907a262ebe1f495
Minesh_Parekh16 <mineshpa@tblocks.com>
2023-03-10 01:17:47 +0530

    react-swipeable changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,45 +1,12 @@
-import React,{useState} from "react";
-import './home.css';
-import PullToRefresh from 'react-pull-to-refresh';
-import axios from "axios";
+import React from "react";
+import {useSwipeable} from 'react-swipeable';
 
 const Home = () => {
 
-    const [dummyData, setDummyData] = useState('Minesh');
-
-    const getData = (url) => {
-        return axios.get(url);
-    }
-
-    const apiData = async () => {
-        await getData('https://jsonplaceholder.typicode.com/posts').then((data) => {
-            setDummyData(data?.data)
-        })
-    }
-
-    const handleRefresh = async () => {
-        await apiData()
-    };
-
-    return (
-        <PullToRefresh onRefresh={handleRefresh} >
-            {typeof(dummyData) === 'object' ?
-                dummyData.map((data,key) =>
-                    <div key={key}>
-                        <div className="dynamic-data" key={key}>
-                            <div>Id:- {data.id}</div>
-                            <div>Body:- {data.body}</div>
-                            <div>Title:- {data.title}</div>
-                            <div>UserId:- {data.userId}</div>
-                        </div>
-                        <hr />
-                    </div>
-                )
-                :
-                <><span className="genericon genericon-next"></span><div className="genericon genericon-next dummy-class">Minesh</div></>
-            }
-        </PullToRefresh>
-    )
+    const handlers = useSwipeable({
+      onSwiped: (eventData) => alert("User Swiped!", eventData),
+    });
+    return <div {...handlers}> You can swipe here </div>;
 }
 
 export default Home
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
